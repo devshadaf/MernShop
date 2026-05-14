f890fe96f8d014f10a9ea056ae60ca84deaf11f5
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-14 22:41:52 +0600

    Remove xss-clean npm package
    Conflict with Code API Response

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express=require("express")
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-const xss = require("xss-clean");
 const hpp = require("hpp");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -15,7 +14,6 @@ const app = express()
 app.use(cookieParser());
 app.use(cors())
 app.use(helmet())
-app.use(xss())
 app.use(hpp())
 app.use(express.json());
 
