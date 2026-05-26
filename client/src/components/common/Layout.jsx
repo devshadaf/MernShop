e71aeaa5a562e97c7a70e556532a1322cd856b68
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-26 15:49:54 +0600

    Toaster Added

--- client/src/components/common/Layout.jsx
+++ client/src/components/common/Layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({children}) => {
-  return <>
-  <Navbar/>
-  {children}
-  <Footer/>
-  </>;
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Toaster position="top-right" reverseOrder={false} />
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
